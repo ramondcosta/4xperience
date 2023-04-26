06ca6d92a93cc060ee1add0fd33f6671e2557b5b
Ramon <ramon.eumsm.costa@gmail.com>
2023-04-26 12:05:28 -0300

    update current health

--- src/app/health-bar/health-bar.component.ts
+++ src/app/health-bar/health-bar.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./health-bar.component.scss']
 })
 export class HealthBarComponent {
+  currentHealth = 75;
 
+  get healthSize() {
+    return this.currentHealth.toString() + "%";
+  }
+  get lostHealthSize() {
+    let lostHealth = 100 - this.currentHealth; 
+    return lostHealth.toString() + "%";
+  }
 }
